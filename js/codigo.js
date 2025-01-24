ea1a93f12b6b88e183ebd8a8eb9bab7bb84fd81b
Caetano Flores <caetano.flores@hotmail.com>
2025-01-24 12:27:48 -0500

    enhancement_fix #1 :Implementacion de funciones async para mejorar el rendimiento

--- js/codigo.js
+++ js/codigo.js
@@ -48,37 +48,37 @@ class Libro{
 
 let libros_disponibles = [];
 let libros_prestados = [];
-function buscar_libro_por_titulo(titulo, libros) { //Cambiamos la variable titulo y el libro.titulo por el autor o genero segun sea necesario
-    // busca un libro por titulo en libros disponibles y regresa el indice
-    // si no lo encuentra regresa -1
-    let libro = libros.filter(libro => libro.titulo === titulo);
-    if (libro.length > 0) {
-        return libros.indexOf(libro[0]);
-    }
-    else
-        return -1;
+async function buscar_libro_por_titulo(titulo, libros) { 
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            for (let i = 0; i < libros.length; i++) {
+                if (libros[i].titulo === titulo) {
+                    resolve(i);
+                    return;
+                }
+            }
+            resolve(-1);
+        }, 0);
+    });
 }
 
-function reservar_libro(titulo){
-    // busca un libro por titulo en libros disponibles
-    // si lo encuentra lo mueve a libros prestados
-    let indice = buscar_libro_por_titulo(titulo, libros_disponibles);
+async function reservar_libro(titulo) {
+    let indice = await buscar_libro_por_titulo(titulo, libros_disponibles);
     if (indice !== -1) {
         libros_prestados.push(libros_disponibles[indice]);
         libros_disponibles.splice(indice, 1);
         const tiempo_prestamo_libro = setTimeout(() => {
             let aviso_devolucion = document.getElementById('aviso_devolucion');
             aviso_devolucion.innerHTML = `Tiene el libro (${titulo}) pendiente de devolucion`;
-          }, 1.296e+9);
+        }, 1.296e+9);
         libros_disponibles[indice].fecha_prestamo = tiempo_prestamo_libro;
         return true;
     }
     return false;
 }
 
-function devolver_libro(titulo){
-    // regresa un libro prestado a libros disponibles
-    let indice = buscar_libro_por_titulo(titulo, libros_prestados);
+async function devolver_libro(titulo) {
+    let indice = await buscar_libro_por_titulo(titulo, libros_prestados);
     if (indice !== -1) {
         libros_disponibles.push(libros_prestados[indice]);
         libros_prestados.splice(indice, 1);
