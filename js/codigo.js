b9bd788b7f54752380fd8240920328ccca5a5c7b
Caetano Flores <caetano.flores@hotmail.com>
2025-01-24 11:52:20 -0500

    item2 - item3 / busquedas e interaccion con el DOM

--- js/codigo.js
+++ js/codigo.js
@@ -7,7 +7,6 @@ class Libro{
 
 let libros_disponibles = [];
 let libros_prestados = [];
-
 function buscar_libro_por_titulo(titulo, libros) { //Cambiamos la variable titulo y el libro.titulo por el autor o genero segun sea necesario
     // busca un libro por titulo en libros disponibles y regresa el indice
     // si no lo encuentra regresa -1
@@ -38,6 +37,23 @@ function devolver_libro(titulo){
         libros_disponibles.push(libros_prestados[indice]);
         libros_prestados.splice(indice, 1);
         return true;
+    }
+}
+
+
+function mostar_libros_disponibles(){
+    const doc_libros_disponibles = document.getElementById('libros_disponibles');
+    libros_disponibles.innerHTML = '';
+    for (const libro of libros_disponibles) {
+        doc_libros_disponibles.innerHTML += `<li>${libro.titulo}</li>`;
+    }
+}
+
+function mostrar_libros_prestados(){
+    const doc_libros_prestados = document.getElementById('libros_prestados');
+    libros_prestados.innerHTML = '';
+    for (const libro of libros_prestados) {
+        doc_libros_prestados.innerHTML += `<li>${libro.titulo}</li>`;
     }
 }
 
